47ec448868302a6e1bcefb15cedab8bff87ef197
Ryan Franklin <ryanfranklin2001@gmail.com>
2026-08-21 19:25:12 -0300

    Atualizando login.js

--- Frontend/JS/Login.js
+++ Frontend/JS/Login.js
@@ -1,41 +1,29 @@
-
 const form = document.querySelector("form");
 
 form.addEventListener("submit", async (e) => {
-
     e.preventDefault();
-
 
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
-    try {
+    const resposta = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email,
+            senha
+        })
+    });
 
-        const resposta = await fetch("http://localhost:3000/api/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email,
-                senha
-            })
-        });
+    const json = await resposta.json();
 
-
-        const json = await resposta.json();
-
-
-        if (resposta.ok) {
-
-            localStorage.setItem("token", json.token);
-            alert("Login realizado");
-            window.location.href = "../home.html";
-        } else {
-
-            alert(json.message);
-        }
-    } catch (erro) {
-        console.error("Erro na requisição:", erro);
+    if (resposta.ok) {
+        localStorage.setItem("token", json.token);
+        alert("Login realizado");
+        window.location.href = "../home.html";
+    } else {
+        alert(json.message);
     }
 });
